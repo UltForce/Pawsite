6dfb0148b80f0502aa98565c86f05df0fa5b77d1
Carlos Vega <carlosvincent.vega.cics@ust.edu.ph>
2024-03-03 21:44:46 +0800

    fixed an error on notifications.js

--- src/Notifications.js
+++ src/Notifications.js
@@ -1,35 +1,13 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-// Notifications.js
-
-import React from "react";
->>>>>>> c3b77fe36f757bda69e29e8f6af860ae7f9b9d4f
-
 
 const Notifications = ({ message }) => {
   return (
-<<<<<<< HEAD
-    <div style={{ 
-      position: 'absolute',
-      top: 0,
-      width: '100%',
-      backgroundColor: 'red',
-      color: 'white',
-      padding: '10px',
-      textAlign: 'center',
-      zIndex: '999',
-    }}>
-      {message}
-=======
     <div className="centered">
       <h2>Notifications</h2>
       <p>Welcome to the Notifications!</p>
->>>>>>> c3b77fe36f757bda69e29e8f6af860ae7f9b9d4f
+      <p>{message}</p>
     </div>
   );
 };
-
-
 
 export default Notifications;
