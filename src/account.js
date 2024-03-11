09149ff0dc8bcf518c92014f651c7d780b18e7bb
Dusk Cantuba <duskdenilson.cantuba.cics@ust.edu.ph>
2024-03-12 05:31:20 +0800

    Mainly added background image (2)

--- src/account.js
+++ src/account.js
@@ -4,10 +4,12 @@ import React from "react";
 
 const account = () => {
   return (
-    <div className="centered">
-      <h1 className="page-title">Account</h1>
-      <p class="lead">Welcome to the account!</p>
-    </div>
+    <section className="background-image">
+      <div className="centered">
+        <h1 className="page-title">Account</h1>
+        <p class="lead">Welcome to the account!</p>
+      </div>
+    </section>
   );
 };
 
